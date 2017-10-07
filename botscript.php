0fab0a389fa522b3b4c19d9c1d75180fb28747b0
DavidClrk <muizzabdulla@gmail.com>
2017-10-07 12:57:52 +0500

    Update botscript.php

--- botscript.php
+++ botscript.php
@@ -3,7 +3,7 @@
 $botToken = "463812915:AAEEa-5oWmSsGhxWsLMBJEfah6eZzKL7LMs";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents($website."/getupdates");
+$update = file_get_contents("php://input");
 
 $updateArray = json_decode($update, TRUE);
 
